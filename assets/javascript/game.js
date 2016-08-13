f9e635c2432c29401bdf9b2f0a7d41f9285bb9da
Corey H <rewer6@gmail.com>
2016-08-13 00:29:46 -0500

    added settings, adding sounds

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,239 +1,432 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  // Hangman
-  var hangman = {
+    // Hangman
+    var hangman = {
 
-    wins : 0,
-    losses : 0,
-    wordList : [],
+        wins: 0,
+        losses: 0,
+        wordList: [],
+        music: true,
 
-    start : function() {
+        start: function() {
 
-      this.word = '';
-      this.wordInProgress = [];
-      this.userGuess = '';
-      this.alreadyGuessed = [];
-      this.chances = 11;
+            this.word = '';
+            this.wordInProgress = [];
+            this.userGuess = '';
+            this.alreadyGuessed = [];
+            this.chances = parseInt(this.getSelectValue('chances-num'));
+            this.wordLength = parseInt(this.getSelectValue('word-length'));
+            this.difficulty = this.chances > 11 ? 'easy' : 'normal';
 
-      this.animation.clear();
+            this.animation.clear();
 
-      if(this.wordList.length < 1) {
-        this.getList();
-      } else {
-        hangman.generateWord();
-      }
+            if (this.wordList.length < 1) {
+                this.getList();
+            } else {
+                this.generateWord();
+            }
 
-    },
+            this.keyOn();
 
-    getList : function(generateWord) {
+        },
 
-      var xhr = new XMLHttpRequest();
+        getList: function() {
 
-      xhr.open('GET', 'assets/words.txt', true);
-      xhr.onreadystatechange = function() {
+            var xhr = new XMLHttpRequest();
 
-        if(xhr.readyState === 4 ) {
+            xhr.open('GET', 'assets/words.txt', true);
+            xhr.onreadystatechange = function() {
 
-          hangman.wordList = xhr.responseText.split(/\s+/);
-          hangman.generateWord();
+                if (xhr.readyState === 4) {
 
-        }
+                    hangman.wordList = xhr.responseText.split(/\s+/);
+                    hangman.generateWord();
 
-      };
+                }
 
-      xhr.send();
+            };
 
-    },
+            xhr.send();
 
-    generateWord : function() {
+        },
 
-      // Generate a random index
-      var rand = Math.floor(Math.random()*this.wordList.length);
-      this.word = this.wordList[rand];
-      console.log(this.word);
+        generateWord: function() {
 
-      // Create HTML skeleton
-      this.createHTML();
+            if(this.wordLength > 0) {
 
-    },
+              var acceptableWords = [];
 
-    createHTML : function() {
+              for(var i=0; i < this.wordList.length; i++) {
 
-      var ulWord = document.getElementById('hangman-word');
-      var ulGuessed = document.getElementById('hangman-guessed');
-      var divChances = document.getElementById('hangman-chances');
+                if(this.wordList[i].length === this.wordLength) {
+                  acceptableWords.push(this.wordList[i]);
+                }
 
-      ulWord.innerHTML = '';
-      ulGuessed.innerHTML = '';
-      divChances.innerHTML = this.chances;
+              }
 
-      for(var i=0; i < this.word.length; i++) {
+              this.word = this.getRandom(acceptableWords);
 
-        var li = document.createElement("LI");
-        var text = document.createTextNode('\u00A0');
-        li.appendChild(text);
-        ulWord.appendChild(li);
+            } else {
 
-      }
-    },
+              this.word = this.getRandom(this.wordList);
 
-    checkBadInput : function(keyInput) {
+            }
 
-      // Check keycodes a-z
-      if(keyInput >= 65 && keyInput <= 90) {
+            console.log(this.word);
 
-        return false;
+            // Create HTML skeleton
+            this.createHTML();
 
-      } else {
+        },
 
-        return true;
+        createHTML: function() {
 
-      }
+            var ulWord = document.getElementById('hangman-word');
+            var ulGuessed = document.getElementById('hangman-guessed');
+            var divChances = document.getElementById('hangman-chances');
 
-    },
+            ulWord.innerHTML = '';
+            ulGuessed.innerHTML = '';
+            divChances.innerHTML = this.chances;
 
-    checkIfGuessed : function() {
+            for (var i = 0; i < this.word.length; i++) {
 
-      if(this.alreadyGuessed.indexOf(this.userGuess) !== -1) {
+                var li = document.createElement("LI");
+                var text = document.createTextNode('\u00A0');
+                li.appendChild(text);
+                ulWord.appendChild(li);
 
-        return true;
+            }
+        },
 
-      } else {
+        getSelectValue: function(id) {
 
-        this.alreadyGuessed.push(this.userGuess);
-        return false;
+          var el = document.getElementById(id);
+          return el.options[el.selectedIndex].value;
 
-      }
+        },
 
-    },
+        getRandom: function(arr) {
 
-    checkInWord: function() {
+          var rand =  Math.floor(Math.random()*arr.length);
+          return arr[rand];
+        },
 
-      var oldWordLength = this.wordInProgress.length;
+        checkBadInput: function(keyInput) {
 
-      for(var i=0; i < this.word.length; i++) {
+            // Check keycodes a-z
+            if (keyInput >= 65 && keyInput <= 90) {
 
-        if(this.userGuess === this.word[i]) {
+                return false;
 
-          this.wordInProgress.push(this.userGuess);
-          this.addLetterHTML(i);
+            } else {
 
-        }
-      }
+                return true;
 
-      if(this.wordInProgress.length === this.word.length) {
+            }
 
-        // Trigger win
-        this.win();
+        },
 
-      } else if(this.wordInProgress.length > oldWordLength) {
+        checkIfGuessed: function() {
 
+            if (this.alreadyGuessed.indexOf(this.userGuess) !== -1) {
 
-        // Play sound
+                return true;
 
-      } else {
+            } else {
 
-        // Remove guess chance
-        this.removeChance();
-      }
+                this.alreadyGuessed.push(this.userGuess);
+                return false;
 
-      this.addGuessedHTML();
+            }
 
-    },
+        },
 
-    addLetterHTML : function(index) {
+        checkInWord: function() {
 
-      var li = document.getElementById('hangman-word').childNodes[index];
+            var oldWordLength = this.wordInProgress.length;
 
-      li.innerHTML = this.userGuess;
+            for (var i = 0; i < this.word.length; i++) {
 
-    },
+                if (this.userGuess === this.word[i]) {
 
-    addGuessedHTML : function() {
+                    this.wordInProgress.push(this.userGuess);
+                    this.addLetterHTML(i);
 
-      var ul = document.getElementById('hangman-guessed');
-      var li = document.createElement("LI");
-      var text = document.createTextNode(this.userGuess);
+                }
+            }
 
-      li.appendChild(text);
-      ul.appendChild(li);
+            if (this.wordInProgress.length === this.word.length) {
 
-    },
+                // Trigger win
+                this.win();
 
-    removeChance : function() {
+            } else if (this.wordInProgress.length > oldWordLength) {
 
-      this.chances--;
-      this.animation.load();
 
-      var div = document.getElementById("hangman-chances");
-      div.innerHTML = this.chances;
+                // Play sound
 
-      // Check if lost
-      if(this.chances < 1)
-        this.lose();
-    },
+            } else {
 
-    win : function() {
-      alert('Congratulations! You win! The word was '+this.word+'!');
-    },
+                // Remove guess chance
+                this.removeChance();
 
-    lose : function() {
-      alert('Better luck next time! The word was '+this.word+'!');
-    },
+                // Check if lost, trigger loss
+                if (this.chances < 1)
+                    this.lose();
 
-    animation: {
-      load: function() {
+            }
 
-        var parts = ['bottom', 'pillar', 'support', 'top', 'noose', 'head', 'torso', 'leftarm', 'rightarm', 'leftleg', 'rightleg'];
-        var index = parts.length - hangman.chances - 1;
+            this.addGuessedHTML();
 
-        document.getElementById('hangman-'+parts[index]).style.opacity = 1;
+        },
 
-      },
-      clear: function() {
+        addLetterHTML: function(index) {
 
-        var divs = document.getElementById('hangman-platform').children;
+            var li = document.getElementById('hangman-word').childNodes[index];
 
-        for(var i=0; i < divs.length; i++) {
-          divs[i].style.opacity = 0;
-        }
+            li.innerHTML = this.userGuess;
 
-      }
-    }
-  };
+        },
 
-  // Create object properties
-  hangman.start();
+        addGuessedHTML: function() {
 
-  // Key press event
-  document.onkeyup = function(event){
+            var ul = document.getElementById('hangman-guessed');
+            var li = document.createElement("LI");
+            var text = document.createTextNode(this.userGuess);
 
-    // Check if input is not an alphabetic character
-    if(hangman.checkBadInput(event.keyCode)) {
-      alert('That\'s not a letter!');
-      return;
-    } else {
-      hangman.userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    }
+            li.appendChild(text);
+            ul.appendChild(li);
 
-    // Check if user already tried that letter
-    if(hangman.checkIfGuessed()) {
-      alert('You\'ve already guessed that letter!');
-      return;
-    }
+        },
 
-    // Check if letter is in the word
-    hangman.checkInWord();
+        removeChance: function() {
 
-  };
+            this.chances--;
+            this.animation.load();
 
-  // Event listeners
-  document.getElementById('hangman-generate').addEventListener('click', function(event) {
+            var div = document.getElementById("hangman-chances");
+            div.innerHTML = this.chances;
 
-    event.preventDefault();
+        },
+
+        win: function() {
+
+            this.wins++;
+            document.getElementById('hangman-wins').innerHTML = this.wins;
+            this.modal.html('<h1 class="text-center">YOU WIN</h1>');
+        },
+
+        lose: function() {
+
+            this.losses++;
+            document.getElementById('hangman-losses').innerHTML = this.losses;
+            this.modal.html('<h1 class="text-center">YOU LOSE!</h1>');
+        },
+
+        keyOn: function() {
+
+            // Key press event
+            document.onkeyup = function(event) {
+
+                // Check if input is not an alphabetic character
+                if (hangman.checkBadInput(event.keyCode)) {
+                    alert('That\'s not a letter!');
+                    return;
+                } else {
+                    hangman.userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+                }
+
+                // Check if user already tried that letter
+                if (hangman.checkIfGuessed()) {
+                    alert('You\'ve already guessed that letter!');
+                    return;
+                }
+
+                // Check if letter is in the word
+                hangman.checkInWord();
+
+            };
+
+        },
+
+        keyOff: function() {
+            document.onkeyup = '';
+        },
+
+        animation: {
+
+            load: function() {
+
+                var reverse;
+
+                if(hangman.difficulty === 'easy' && hangman.chances % 2 !== 0){
+                  return;
+
+                } else if(hangman.difficulty === 'easy' && hangman.chances % 2 === 0) {
+                  reverse = hangman.chances/2;
+
+                } else {
+                  reverse = hangman.chances;
+                }
+
+                var parts = ['bottom', 'pillar', 'support', 'top', 'noose', 'head', 'torso', 'leftarm', 'rightarm', 'leftleg', 'rightleg'];
+                var index = parts.length - reverse - 1;
+
+                document.getElementById('hangman-' + parts[index]).style.opacity = 1;
+
+            },
+            clear: function() {
+
+                var divs = document.getElementById('hangman-platform').children;
+
+                for (var i = 0; i < divs.length; i++) {
+                    divs[i].style.opacity = 0;
+                }
+
+            }
+
+        },
+
+        sound: {
+
+          mute: false,
+          winTracks: ['ff7victory.mp3'], // Plays on win
+          loseTracks: ['sadviolinmlg.mp3','heartwillgoon.mp3'], // Plays on loss
+          rightTracks: [],
+          wrongTracks: [],
+          path: 'assets/sounds',
+          bgMusic: document.getElementById('hangman-music-bg'),
+
+          init: function() {
+
+            this.allTracks = [];
+
+            this.winSound = hangman.getRandom(this.winTracks);
+            this.loseSound = hangman.getRandom(this.loseTracks);
+            this.rightNoise = hangman.getRandom(this.rightTracks);
+            this.wrongNoise = hangman.getRandom(this.wrongTracks);
+
+            this.bgMusic.play();
+
+          },
+
+          toggleMute: function(el) {
+
+            console.log(el.children);
+            this.mute = this.mute ? false : true;
+
+            var icon = this.mute ? 'glyphicon glyphicon-volume-off' : 'glyphicon  glyphicon-volume-up';
+
+            this.bgMusic.muted = this.mute;
+            el.className = icon;
+
+
+          },
+
+          right: function() {
+
+          },
+          wrong: function() {
+
+          },
+          victory: function() {
+
+          },
+          death: function() {
+
+          },
+          stop: function() {
+
+          }
+
+        },
+
+        modal: {
+
+            open: function() {
+
+                var container = document.getElementById('modal-container');
+                container.style.display = 'initial';
+                container.style.opacity = 1;
+
+                hangman.keyOff();
+
+            },
+
+            close: function() {
+
+                var container = document.getElementById('modal-container');
+                var box = document.getElementById('modal-content');
+                container.style.display = 'none';
+                container.style.opacity = 0;
+                box.innerHTML = '';
+
+                hangman.keyOn();
+
+            },
+
+            html: function(content) {
+
+                var box = document.getElementById('modal-content');
+                box.innerHTML = content;
+                this.open();
+
+            }
+
+        },
+
+    };
+
+    // Let the games begin
+    hangman.modal.html('<h1>Wecome to Hangman</h1><p>Your goal is to guess the word shown by underlining dashes. <br /><br />Press any alphabetic key to help guess the word. If you fail, a piece of the hangman will appear.  If all pieces assemble, the game is over and you lose.</p><h4>Save the hangman and you win!</h4>');
     hangman.start();
 
-  });
+    // Event listeners
+    document.getElementById('hangman-generate').addEventListener('click', function(event) {
+
+        event.preventDefault();
+        hangman.start();
+
+    });
+
+    document.querySelector('#hangman-sound > span').addEventListener('click', function(event) {
+
+        event.preventDefault();
+        hangman.sound.toggleMute(event.target);
+
+    });
+
+    document.getElementById('hangman-settings').addEventListener('click', function(event) {
+
+        event.preventDefault();
+        hangman.modal.open();
+
+    });
+
+    // Modal
+
+    document.getElementById('modal-close').addEventListener('click', function(event) {
+
+        event.preventDefault();
+        hangman.modal.close();
+
+    });
+
+    document.getElementById('start-over').addEventListener('click', function(event) {
+
+        event.preventDefault();
+        hangman.start();
+        hangman.modal.close();
+
+    });
+
+    document.getElementById('modal-overlay').addEventListener('click', function(event) {
+
+        event.preventDefault();
+        hangman.modal.close();
+
+    });
 
 })();
